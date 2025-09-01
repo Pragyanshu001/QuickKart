5d374b281abac9fe313f9514701c5063f5a31eaf
Pragyanshu Gupta <117273832+Pragyanshu001@users.noreply.github.com>
2025-09-02 01:55:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: [
-      "http://localhost:5174",
+      "https://quickkart-frontend.onrender.com",
       "http://localhost:5175",
       "http://localhost:5173",
     ],
