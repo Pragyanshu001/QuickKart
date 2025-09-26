9d5f1f2a1a53d40e57212fcc3de2293f6be4cc24
Pragyanshu Gupta <117273832+Pragyanshu001@users.noreply.github.com>
2025-09-26 13:45:09 +0530

    Update Registration.jsx

--- frontend/src/pages/Registration.jsx
+++ frontend/src/pages/Registration.jsx
@@ -6,7 +6,7 @@ import { IoEyeOutline } from "react-icons/io5";
 import { IoEye } from "react-icons/io5";
 import { useState } from "react";
 import { useContext } from "react";
-import { authDataContext } from "../context/AuthContext";
+import { authDataContext } from "../context/authContext";
 import axios from "axios";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../utils/Firebase";
@@ -22,6 +22,7 @@ function Registration() {
   let [password, setPassword] = useState("");
   let { userdata, getCurrentUser } = useContext(userDataContext);
   let [loading, setLoading] = useState(false);
+  let [passwordMsg, setPasswordMsg] = useState("");
 
   let navigate = useNavigate();
 
@@ -39,12 +40,19 @@ function Registration() {
         { withCredentials: true }
       );
       getCurrentUser();
+
+      sessionStorage.setItem("justLoggedIn", "true");
+
       navigate("/");
       toast.success("User Registration Successful");
       console.log(result.data);
       setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
+      setName("");
+      setEmail("");
+      setPassword("");
       toast.error("User Registration Failed");
     }
   };
@@ -63,6 +71,7 @@ function Registration() {
       );
       console.log(result.data);
       getCurrentUser();
+      sessionStorage.setItem("justLoggedIn", "true");
       navigate("/");
       toast.success("User Registration Successful");
     } catch (error) {
@@ -72,42 +81,41 @@ function Registration() {
   };
 
   return (
-    <div className="w-[100vw] h-[100vh] bg-gradient-to-l from-[#141414] to-[#0c2025] text-[white] flex flex-col items-center justify-start">
+    <div className="w-[100vw] h-[100vh] bg-gradient-to-l from-[#d4a465] to-[#ab8758] text-[white] flex flex-col items-center justify-start">
       <div
         className="w-[100%] h-[80px] flex items-center justify-start px-[30px] gap-[10px] cursor-pointer"
         onClick={() => navigate("/")}
       >
-        <img className="w-[40px]" src={Logo} alt="" />
-        <h1 className="text-[22px] font-sans ">OneCart</h1>
+        <img className="w-[130px]" src={Logo} alt="" />
       </div>
 
       <div className="w-[100%] h-[100px] flex items-center justify-center flex-col gap-[10px]">
-        <span className="text-[25px] font-semibold">Registration Page</span>
-        <span className="text-[16px]">
-          Welcome to OneCart, Place your order
+        <span className="mast text-[30px] font-semibold">Login Page</span>
+        <span className="darkText text-[16px]">
+          Welcome to QuickKart, Place your order
         </span>
       </div>
-      <div className="max-w-[600px] w-[90%] h-[500px] bg-[#00000025] border-[1px] border-[#96969635] backdrop:blur-2xl rounded-lg shadow-lg flex items-center justify-center ">
+      <div className="min-w-[350px] w-[35%] h-[65%] text-black bg-[#f5f5f2a4] border-[1px] border-[#96969635] backdrop:blur-2xl rounded-lg shadow-lg flex items-center justify-center ">
         <form
           action=""
           onSubmit={handleSignup}
           className="w-[90%] h-[90%] flex flex-col items-center justify-start gap-[20px]"
         >
           <div
-            className="w-[90%] h-[50px] bg-[#42656cae] rounded-lg flex items-center justify-center gap-[10px] py-[20px] cursor-pointer"
+            className="w-[90%] h-[50px] bg-[#754F23] rounded-lg flex items-center justify-center gap-[10px] py-[20px] cursor-pointer"
             onClick={googleSignup}
           >
             <img src={google} alt="" className="w-[20px]" /> Registration with
             Google
           </div>
           <div className="w-[100%] h-[20px] flex items-center justify-center gap-[10px]">
-            <div className="w-[40%] h-[1px] bg-[#96969635]"></div> OR{" "}
-            <div className="w-[40%] h-[1px] bg-[#96969635]"></div>
+            <div className="w-[40%] h-[3px] bg-[#754F23]"></div> OR{" "}
+            <div className="w-[40%] h-[3px] bg-[#754F23]"></div>
           </div>
-          <div className="w-[90%] h-[400px] flex flex-col items-center justify-center gap-[15px]  relative">
+          <div className="w-[90%] h-[300px] flex flex-col items-center justify-center gap-[15px]  relative">
             <input
               type="text"
-              className="w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop:blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold"
+              className="w-[100%] h-[50px] bg- border-[2px] border-[#754F23] backdrop:blur-sm rounded-lg shadow-lg bg-[#dfc9ac] placeholder-[#ffffffc7] px-[20px] font-semibold"
               placeholder="UserName"
               required
               onChange={(e) => setName(e.target.value)}
@@ -115,7 +123,7 @@ function Registration() {
             />
             <input
               type="text"
-              className="w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop:blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold"
+              className="w-[100%] h-[50px] border-[2px] border-[#754F23] backdrop:blur-sm rounded-lg shadow-lg bg-[#dfc9ac] placeholder-[#ffffffc7] px-[20px] font-semibold"
               placeholder="Email"
               required
               onChange={(e) => setEmail(e.target.value)}
@@ -123,12 +131,25 @@ function Registration() {
             />
             <input
               type={show ? "text" : "password"}
-              className="w-[100%] h-[50px] border-[2px] border-[#96969635] backdrop:blur-sm rounded-lg shadow-lg bg-transparent placeholder-[#ffffffc7] px-[20px] font-semibold"
+              className="w-[100%] h-[50px] border-[2px] border-[#754F23] backdrop:blur-sm rounded-lg shadow-lg bg-[#dfc9ac] placeholder-[#ffffffc7] px-[20px] font-semibold"
               placeholder="Password"
               required
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={(e) => {
+                setPassword(e.target.value);
+                if (e.target.value.length < 8) {
+                  setPasswordMsg("Password must be at least 8 characters");
+                } else {
+                  setPasswordMsg("");
+                }
+              }}
               value={password}
             />
+            {passwordMsg && (
+              <p className="text-red-600 text-sm font-semibold absolute bottom-[6.5rem] left-[1rem]">
+                {passwordMsg}
+              </p>
+            )}
+
             {!show && (
               <IoEyeOutline
                 className="w-[20px] h-[20px] cursor-pointer absolute right-[5%]"
@@ -141,9 +162,15 @@ function Registration() {
                 onClick={() => setShow((prev) => !prev)}
               />
             )}
-            <button className="w-[100%] h-[50px] bg-[#6060f5] rounded-lg flex items-center justify-center mt-[20px] text-[17px] font-semibold">
-              {loading ? <Loading /> : "Create Account"}
-            </button>
+            {password.length >= 8 ? (
+              <button className="dark w-[100%] h-[50px] rounded-lg flex items-center justify-center mt-[20px] text-[17px] font-semibold">
+                {loading ? <Loading /> : "Create Account"}
+              </button>
+            ) : (
+              <span className="dark w-[100%] h-[50px] rounded-lg flex items-center justify-center mt-[20px] text-[17px] font-semibold">
+                Create Account
+              </span>
+            )}
             <p className="flex gap-[10px]">
               You have any account?{" "}
               <span
