3ed55702a41b49bea05c97ad06b0b15e5aa67d06
Pragyanshu Gupta <117273832+Pragyanshu001@users.noreply.github.com>
2025-09-26 13:48:48 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,26 @@
-import React from "react";
-import LoginAnimation from "../components/LoginAnimation";
+import React, { useEffect, useState } from "react";
 import Hero from "../components/Hero";
 import { FlipWords } from "../components/FlipWord";
 import Product from "./Product";
 import OurPolicy from "../components/OurPolicy";
 import NewLetterBox from "../components/NewLetterBox";
 import Footer from "../components/Footer";
+import LoginAnimation from "../components/LoginAnimation";
 
 const Home = () => {
+  const [showAnimation, setShowAnimation] = useState(false);
+
+  useEffect(() => {
+    // check session flag
+    if (sessionStorage.getItem("justLoggedIn") === "true") {
+      setShowAnimation(true);
+      sessionStorage.removeItem("justLoggedIn"); // taaki dobara reload pe na chale
+    }
+  }, []);
+
   return (
     <>
-      <LoginAnimation /> 
+      {showAnimation && <LoginAnimation />}
 
       <div className="text bg-gradient-to-l overflow-y-hidden w-[100vw] h-screen from-[#d4a465]  to-[#ab8758] md:flex items-center overflow-x-hidden justify-start">
         <div className="relative md:w-1/2 h-30 top-30 justify-center flex md:h-[100vh] ">
@@ -18,7 +28,7 @@ const Home = () => {
             <p className="text-2xl top-1 md:top-10 sm:text-5xl md:text-6xl">
               Wellcome to QuickKart
             </p>
-            <div className="flex text-amber-400 text-5xl lg:text-3xl mt-2 items-baseline ">
+            <div className="flex text-amber-400 z-0 text-5xl lg:text-3xl mt-2 items-baseline ">
               <p className="text-3xl md:text-4xl  text-[#1E1E1E] ml-5">
                 {" "}
                 To Explore ,{" "}
